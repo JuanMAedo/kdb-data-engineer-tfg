83067869ee6ea9714c4e7014406950601deef3cd
JuanMAedo <juan.martinez.aedo.g@gmail.com>
2024-05-11 15:51:23 +0200

    fixed scripts

--- Data/script_etf_a_csv.py
+++ Data/script_etf_a_csv.py
@@ -5,10 +5,10 @@ import openpyxl
 
 # Control de argumentos y recogida de excels de la ruta dada de entrada
 parser = argparse.ArgumentParser()
-parser.add_argument("excel_path", type=str)
-parser.add_argument("excel_name", type=str)
+parser.add_argument('excel_path', type=str)
+parser.add_argument('excel_name', type=str)
 args = parser.parse_args()
-all_etf_excel = [f for f in os.listdir(args.excel_path) if f.endswith(".xlsx")]
+all_etf_excel = [f for f in os.listdir(args.excel_path) if f.endswith('.xlsx')]
 
 # Cargar el archivo de búsqueda y creamos diccionario
 wb_search = openpyxl.load_workbook(args.excel_name)
@@ -25,37 +25,28 @@ wb_search.close()
 for etf_excel in all_etf_excel:
 
     excel_path_etf = os.path.join(args.excel_path, etf_excel)
-    df = pd.read_excel(excel_path_etf, sheet_name="Historical")
+    df = pd.read_excel(excel_path_etf, sheet_name='Historical')
     wb = openpyxl.load_workbook(excel_path_etf)
 
     # Limpiamos datos y renombramos
-    df = df.drop(columns=["Ex-Dividends", "Non-FV NAV"])
-    df = df.rename(
-        columns={
-            "As Of": "date",
-            "NAV per Share": "nav_value",
-            "Shares Outstanding": "shares",
-        }
-    )
-    df["date"] = pd.to_datetime(df["date"])
+    df = df.drop(columns=['Ex-Dividends', 'Non-FV NAV'])
+    df = df.rename(columns={'As Of': 'date', 'NAV per Share': 'nav_value', 'Shares Outstanding': 'shares'})
+    df['date'] = pd.to_datetime(df['date'])
 
+    # Guardar el valor anterior
     previous_value = None
+    
     # Iterar sobre las filas y rellenar valores faltantes en la columna 'shares'
     for index, row in df.iterrows():
-        if row["shares"] == "--" and previous_value is not None:
-            df.at[index, "shares"] = previous_value
+        if row['shares'] == "--" and previous_value is not None:
+            df.at[index, 'shares'] = previous_value
         else:
-            previous_value = row["shares"]
+            previous_value = row['shares']
 
     # Exportar el DataFrame a un archivo CSV
-    etf_name = wb["Holdings"]["A2"].value.strip()
-    df.to_csv(
-        os.path.join(
-            "Data/pre-processed/etf/",
-            etf_tickers_dicc.get(etf_name, etf_name) + ".csv",
-            index=False,
-            sep=",",
-            header=True,
-        )
-    )
-    print(f"Se ha exportado el archivo CSV como '{etf_name}.csv'")
+    etf_name = wb['Holdings']['A2'].value.strip()  
+    csv_path_saving = os.path.join("Data/pre-processed/etf/", etf_tickers_dicc.get(etf_name, etf_name) + ".csv")
+    df.to_csv(csv_path_saving, index=False, sep=',', header=True)
+        
+    print(f'Se ha exportado el archivo CSV como \'{csv_path_saving}\'')
+
--- Data/script_resume_etf.py
+++ Data/script_resume_etf.py
@@ -3,27 +3,22 @@ import pandas as pd
 
 # Control de argumentos y recogida de excels de la ruta dada de entrada
 parser = argparse.ArgumentParser()
-parser.add_argument("excel_path", type=str)
+parser.add_argument('excel_path', type=str)
 args = parser.parse_args()
 
-# Limpiamos datos y renombramos
+# Cargar el excel resumen como DataFrame
 df = pd.read_excel(args.excel_path, skiprows=1)
-df = df[
-    "Ticker", "Incept. Date", "Net Assets (USD)", "Sub Asset Class", "Region", "Market"
-]
-df = df.rename(
-    columns={
-        "Ticker": "ticker",
-        "Incept. Date": "inception_date",
-        "Net Assets (USD)": "net_assets",
-        "Sub Asset Class": "equity_class",
-        "Region": "region",
-        "Market": "market",
-    }
-)
-df["inception_date"] = pd.to_datetime(df["inception_date"])
+
+# Limpiamos datos y renombramos
+columns_to_csv = ['Ticker', 'Incept. Date', 'Net Assets (USD)', 'Sub Asset Class', 'Region', 'Market']  # Columnas que deseas conservar
+df = df[columns_to_csv]  # Selecciona solo las columnas que deseas conservar
+df = df.rename(columns={'Ticker': 'ticker', 'Incept. Date': 'inception_date', 
+                        'Net Assets (USD)': 'net_assets', 'Sub Asset Class': 'equity_class',
+                        'Region': 'region', 'Market': 'market'})
+df['inception_date'] = pd.to_datetime(df['inception_date'])
 
 # Exportar el DataFrame a un archivo CSV
-df.to_csv("Data/pre-processed/resume.csv", index=False, sep=",", header=True)
+df.to_csv("Data/pre-processed/resume.csv", index=False, sep=',', header=True)
 
-print(f"Se ha exportado el archivo CSV como 'Data/pre-processed/resume.csv'")
+print(f'Se ha exportado el archivo CSV como \'Data/pre-processed/resume.csv\'')
+
